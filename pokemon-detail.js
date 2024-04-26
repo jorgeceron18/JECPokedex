e9bf86e914aece05bbaf89984d4c3eae23cd4d57
Jorge <jorgeceron18@hotmail.com>
2024-04-26 14:40:44 -0500

    Actualización en pokemon details

---
+++ pokemon-detail.js
@@ -0,0 +1,23 @@
+let currentPokemonId = null;
+
+document.addEventListener("DOMContentLoaded", () => {
+  const MAX_POKEMONS = 151;
+  const pokemonID = new URLSearchParams(window.location.search).get("id");
+  const id = parseInt(pokemonID, 10);
+
+  if (id < 1 || id > MAX_POKEMONS) {
+    return (window.location.href = "./index.html");
+  }
+
+  currentPokemonId = id;
+  loadPokemon(id);
+});
+
+async function loadPokemon(id) {
+  try {
+    return true;
+  } catch (error) {
+    console.error("An error occurred while fetching Pokemon data:", error);
+    return false;
+  }
+}
