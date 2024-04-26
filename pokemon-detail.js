55694ef5ac67e9ee0e4d95902c656d8f2c734978
Jorge <jorgeceron18@hotmail.com>
2024-04-26 15:24:02 -0500

    Pokemon detail js update

--- pokemon-detail.js
+++ pokemon-detail.js
@@ -15,9 +15,103 @@ document.addEventListener("DOMContentLoaded", () => {
 
 async function loadPokemon(id) {
   try {
+    const [pokemon, pokemonSpecies] = await Promise.all([
+      fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) =>
+        res.json()
+      ),
+      fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then((res) =>
+        res.json()
+      ),
+    ]);
+
+    const abilitiesWrapper = document.querySelector(
+      ".pokemon-detail-wrap .pokemon-detail.move"
+    );
+    abilitiesWrapper.innerHTML = "";
+
+    if (currentPokemonId === id) {
+      displayPokemonDetails(pokemon);
+      const flavorText = getEnglishFlavorText(pokemonSpecies);
+      document.querySelector("/.body3-fonts.pokemon-description").textContent =
+        flavorText;
+
+      const [leftArrow, rightArrow] = ["#leftArrow", "#rightArrow"].map((sel) =>
+        document.querySelector(sel)
+      );
+      leftArrow.removeEventListener("click", navigatePokemon);
+      rightArrow.removeEventListener("click", navigatePokemon);
+
+      if (id !== 1) {
+        leftArrow.addEventListener("click", () => {
+          navigatePokemon(id - 1);
+        });
+      }
+      if (id !== 151) {
+        rightArrow.addEventListener("click", () => {
+          navigatePokemon(id + 1);
+        });
+      }
+
+      window.history.pushState({}, "", `./detail.html?id=${id}`);
+    }
+
     return true;
   } catch (error) {
     console.error("An error occurred while fetching Pokemon data:", error);
     return false;
   }
+}
+
+async function navigatePokemon(id) {
+  currentPokemonId = id;
+  await loadPokemon(id);
+}
+
+const typeColors = {
+  normal: "#A8A878",
+  fire: "#F08030",
+  water: "#6890F0",
+  electric: "#F8D030",
+  grass: "#78C850",
+  ice: "#98D8D8",
+  fighting: "#C03028",
+  poison: "#A040A0",
+  ground: "#E0C068",
+  flying: "#A890F0",
+  psychic: "#F85888",
+  bug: "#A8B820",
+  rock: "#B8A038",
+  ghost: "#705898",
+  dragon: "#7038F8",
+  dark: "#705848",
+  steel: "#B8B8D0",
+  dark: "#EE99AC",
+};
+
+function setElementsStyles(elements, cssProperty, value) {
+  elements.forEach((element) => {
+    element.style[cssProperty] = value;
+  });
+}
+
+function rgbaFromHex(hexColor) {
+  return [
+    parseInt(hexColor.slice(1, 3), 16),
+    parseInt(hexColor.slice(3, 5), 16),
+    parseInt(hexColor.slice(5, 7), 16),
+  ].join(", ");
+}
+
+function setTypeBackgroundColor(pokemon) {
+  const mainType = pokemon.types[0].type.name;
+  const color = typeColors[mainType];
+
+  if (!color) {
+    console.warn(`Color not defined for type: ${mainType}`);
+    return;
+  }
+
+  const detailMainElement = document.querySelector(".detail-main");
+  setElementsStyles([detailMainElement], "backgroundColor", color);
+  setElementsStyles([detailMainElement], "borderColor", color);
 }
